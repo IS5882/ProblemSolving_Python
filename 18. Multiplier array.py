e5a85e171058f98ff9b712d4efc95087068eee70
Injy Sarhan <47028011+IS5882@users.noreply.github.com>
2020-11-07 20:09:35 +0100

    Add files via upload

---
+++ 18. Multiplier array.py
@@ -0,0 +1,11 @@
+"""find the longest subarray whose sum is less or equal to K
+
+int[] arr = {1, 2, 1, 0, 1, -8, -9, 0};
+int k = 4;
+Output is 8
+
+int arr[] = {1, 2, 1, 0, 1, 1, 0};
+int k = 4;
+output is 5 """
+
+def longestSub(nums,k):
